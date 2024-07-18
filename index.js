db2a80743d4ac02675293629390e589769235f52
barkley-wq <igonyibarkley00@gmail.com>
2024-07-18 10:28:31 +0300

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Add your code here
+
+
+function submitData (name , email){
+    return fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+    },
+        body: JSON.stringify({name, email}),
+    })
+    .then( (response)=>response.json())
+    .then(function (response) {
+        return document.body.innerHTML = response["id"]
+        })
+    .catch(function (error) {
+    alert(error.message);
+    document.body.innerHTML = error.message;
+      })
+    }
